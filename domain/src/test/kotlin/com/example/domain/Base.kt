c7037efe5c639bba9d4c3266a4153e107c1c4591
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2018-06-24 22:15:09 +0300

    Fixed test dependencies creation

--- domain/src/test/kotlin/com/example/domain/Base.kt
+++ domain/src/test/kotlin/com/example/domain/Base.kt
@@ -1,41 +1,57 @@
 package com.example.domain
 
+import io.reactivex.observers.TestObserver
 import io.reactivex.subjects.ReplaySubject
 import org.jetbrains.spek.api.dsl.SpecBody
 import org.jetbrains.spek.api.dsl.context
-import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.lifecycle.CachingMode
 
 fun <T: UiActor<*, *>> SpecBody.component(componentCreator: () -> T, body: Component<T>.() -> Unit) =
         Component(this, componentCreator).apply {
-            given("a component ...") {
+            context("a component ...") {
                 body()
             }
         }
 
 fun <T: UiActor<*, *>> Component<T>.initialized(cmd: UiCommand, body: InitializedComponent<T>.() -> Unit) =
-        InitializedComponent(this.specBody, cmd, componentCreator).apply {
-            specBody.context("component is initialized") {
-                initialize()
-                body()
-            }
-        }
+        InitializedComponent(this.specBody, cmd, componentCreator).apply(body)
+
+data class Deps(val cmdstr: ReplaySubject<UiCommand>, val sub: TestObserver<UiResult>)
 
 open class Component<T : UiActor<*, *>>(
         val specBody: SpecBody,
         val componentCreator: () -> T
 ) {
 
-    val component by specBody.memoized {
+    /*val component by specBody.memoized {
         println("memo")
         componentCreator()
-    }
-    open val cmdstr by specBody.memoized { ReplaySubject.create<UiCommand>() }
-    val sub by specBody.memoized {
-        (component as UiActor<UiCommand, UiResult>).process(cmdstr).test()
+    }*/
+
+    open val deps by specBody.memoized(CachingMode.TEST) {
+        val cmdstr by specBody.memoized {
+            println("cmdstr")
+            ReplaySubject.create<UiCommand>() }
+        val sub by specBody.memoized {
+            println("sub")
+            (componentCreator() as UiActor<UiCommand, UiResult>).process(cmdstr).test()
+        }
+        Deps(cmdstr, sub)
     }
 
-    fun assertResultAt(index: Int, result: UiResult)
-            = sub.assertValueAt(index, result)
+    /*val cmdstr by specBody.memoized {
+        println("cmdstr")
+        ReplaySubject.create<UiCommand>() }
+    val sub by specBody.memoized {
+        println("sub")
+        (componentCreator() as UiActor<UiCommand, UiResult>).process(cmdstr).test()
+    }*/
+
+    fun assertResultAt(index: Int, result: UiResult): TestObserver<UiResult> {
+        println("assert $result")
+        return deps.sub.assertValueAt(index, result)
+
+    }
 
 }
 
@@ -44,8 +60,16 @@ class InitializedComponent<T : UiActor<*, *>>(
         val cmd: UiCommand,
         val componentCreator1: () -> T) : Component<T>(specBody1, componentCreator1) {
 
+    override val deps by lazy {
+        val d = super.deps
+        d.cmdstr.onNext(cmd)
+        //initialize()
+        d
+    }
+
     fun initialize() {
-        cmdstr.onNext(cmd)
+        println("initialize")
+        deps.cmdstr.onNext(cmd)
     }
 
 }
--- domain/src/test/kotlin/com/example/domain/ClarifyingQuestions2Spec.kt
+++ domain/src/test/kotlin/com/example/domain/ClarifyingQuestions2Spec.kt
@@ -3,6 +3,7 @@ package com.example.domain
 import com.example.domain.models.ItemDetails
 import com.example.domain.models.ItemOpportunity
 import com.example.domain.models.Proposal
+import com.example.domain.models.Question
 import com.example.domain.submitProposal.ClarifyingQuestions
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.it
@@ -16,14 +17,47 @@ class ClarifyingQuestions2Spec : Spek({
             Proposal(0, null)
     )
 
-    component(::ClarifyingQuestions) {
-        initialized(ClarifyingQuestions.Command.INIT(itemOpportunity)) {
+    val questions = listOf(Question("1", "q1"), Question("2", "q1"))
+    val itemOpportunity2 = ItemOpportunity(
+            ItemDetails("1234", questions),
+            Proposal(0, null)
+    )
 
-            on("init") {
-                it("should") {
-                    assertResultAt(0, ClarifyingQuestions.Result.NoQuestions)
+    group("sdaf") {
+
+        component(::ClarifyingQuestions) {
+
+            initialized(ClarifyingQuestions.Command.INIT(itemOpportunity2)) {
+
+                on("init2") {
+                    it("should2") {
+                        assertResultAt(0, ClarifyingQuestions.Result.Questions(questions))
+                    }
                 }
+
+                on("init3") {
+                    it("should3") {
+                        assertResultAt(0, ClarifyingQuestions.Result.Questions(questions))
+                    }
+                }
+
+
             }
+        }
+        component(::ClarifyingQuestions) {
+
+            initialized(ClarifyingQuestions.Command.INIT(itemOpportunity)) {
+
+                on("init") {
+                    it("should") {
+                        assertResultAt(0, ClarifyingQuestions.Result.NoQuestions)
+                    }
+                }
+
+
+            }
+
+
         }
     }
 })
