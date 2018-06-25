a5d97f595c9ef9d539f3f56739180af897f551ae
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2018-06-25 22:37:58 +0300

    Implemented spec for cover letter

--- domain/src/main/kotlin/com/example/domain/submitProposal/CoverLetter.kt
+++ domain/src/main/kotlin/com/example/domain/submitProposal/CoverLetter.kt
@@ -1,12 +1,89 @@
 package com.example.domain.submitProposal
 
-import com.example.domain.UiCommand
-import com.example.domain.UiComponent
-import com.example.domain.UiResult
-import com.example.domain.UiState
+import com.example.domain.*
+import com.example.domain.models.ItemOpportunity
+import com.example.domain.submitProposal.CoverLetter.*
 import io.reactivex.Observable
-import io.reactivex.subjects.PublishSubject
-import java.util.concurrent.TimeUnit
+import io.reactivex.ObservableTransformer
+
+class CoverLetter : UiComponent<Command, Result, ViewState> {
+    override fun process(commands: Observable<Command>): Observable<Result> {
+        return commands
+                .doOnNext { println("CMD " + it) }
+                .compose(coverLetterProcessor)
+                .doOnNext { println("RES " + it) }
+                .cast(Result::class.java)
+    }
+
+    override fun render(): Observable<ViewState> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    val coverLetterProcessor =
+            ObservableTransformer<Command, Result> { t ->
+                t//.debounce(3, TimeUnit.SECONDS)
+                        .map {
+                            when (it) {
+                                is Command.DATA -> {
+                                    if (it.itemOpportunity.itemDetails.isCoverLetterRequired) {
+                                        result(it.itemOpportunity.proposal.coverLetter)
+                                    } else {
+                                        Result.NoCoverLetterRequired
+                                    }
+                                }
+                                is Command.UpdateCoverLetter -> {
+                                    result(it.coverLetter)
+                                }
+                            }
+                        }
+
+            }
+
+    private fun result(coverLetter: String): Result {
+        //save cover letter to submit proposal storage?
+
+        val validated = coverLetter.trim()
+
+        val maxLength = 5000
+        return when {
+            validated.length > maxLength -> Result.LengthExceeded(validated, maxLength)
+            validated.isNotEmpty() -> Result.Valid(validated)
+            else -> Result.Empty
+        }
+    }
+
+
+    sealed class CoverLetterEvents : UiEvent {
+        data class CoverLetterUpdated(val coverLetter: String) : CoverLetterEvents()
+    }
+
+    sealed class Command : UiCommand {
+        data class DATA(val itemOpportunity: ItemOpportunity) : Command(), UiDataCommand
+
+        data class UpdateCoverLetter(val coverLetter: String) : Command()
+    }
+
+    sealed class Result : UiResult {
+
+        object NoCoverLetterRequired : Result()
+
+        sealed class Status: Result() {
+            object Completed: Status()
+            object NotCompleted : Status()
+        }
+
+        data class Valid(val coverLetter: String) : Result()
+        object Empty : Result()
+        data class LengthExceeded(val coverLetter: String, val limit: Int) : Result()
+    }
+
+    data class ViewState(
+            val coverLetter: String = "",
+            val isLengthExceeded: Boolean = false
+    ) : UiState
+
+}
+
 
 /*
 class CoverLetter : UiComponent<> {
@@ -101,10 +178,6 @@ class CoverLetter : UiComponent<> {
             }
 
 
-    data class CoverLetterViewState(
-            val coverLetter: String = "",
-            val isLengthExceeded: Boolean = false
-    ) : UiState
 
 
 }*/
---
+++ domain/src/test/kotlin/com/example/domain/CoverLetterSpec.kt
@@ -0,0 +1,66 @@
+package com.example.domain
+
+import com.example.domain.models.ItemDetails
+import com.example.domain.models.ItemOpportunity
+import com.example.domain.models.Proposal
+import com.example.domain.submitProposal.CoverLetter
+import com.example.domain.submitProposal.CoverLetter.Command.DATA
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.xit
+
+class CoverLetterSpec : Spek({
+
+    val withEmptyCoverLetter = ItemOpportunity(
+            ItemDetails("1234", true, null),
+            Proposal(0, null, "")
+    )
+
+    val withCoverLetterNotRequired = ItemOpportunity(
+            ItemDetails("1234", false, null),
+            Proposal(0, null, "")
+    )
+
+    component(::CoverLetter) {
+
+        initialized(DATA(withEmptyCoverLetter)) {
+
+            describe("client sends DATA with cover letter required") {
+                xit("should emit status as not completed") {
+
+                }
+
+                it("should emit empty cover letter") {
+                    assertResultAt(0, CoverLetter.Result.Empty)
+
+                }
+            }
+
+            describe("client fills in a valid cover letter") {
+                perform {
+                    command(CoverLetter.Command.UpdateCoverLetter("cover letter"))
+                }
+
+                it("should emit valid cover letter") {
+                    assertResultAt(1, CoverLetter.Result.Valid("cover letter"))
+                }
+            }
+        }
+
+        initialized(DATA(withCoverLetterNotRequired)) {
+
+            describe("client sends DATA with cover letter not required") {
+                it("should emit only 'not required' result") {
+                    assertValuesOnly(CoverLetter.Result.NoCoverLetterRequired)
+                }
+            }
+
+            describe("client tries to enter cover letter when it's not required") {
+                xit("should fail with error") {
+
+                }
+            }
+        }
+    }
+})
