184dcb41e98543d1c10397e4e13ff547ef976cb7
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2018-07-03 12:49:27 +0300

    Fixed stupid Android build issue

--- app/src/main/kotlin/com/example/clean/DefaultDataBinder.kt
+++ app/src/main/kotlin/com/example/clean/DefaultDataBinder.kt
@@ -7,7 +7,7 @@ import android.view.View
 class DefaultDataBinder : DataBinder {
     override fun bind(view: View, viewModel: ViewModel) {
         DataBindingUtil.bind<ViewDataBinding>(view).run {
-            setVariable(BR.viewModel, viewModel)
+            //setVariable(BR.viewModel, viewModel)
             executePendingBindings()
         }
     }
