8956ce7d82687006537cd548985a5a4433db5170
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2018-07-06 12:05:44 +0300

    Enabled excess binding execution fora while

--- app/src/main/kotlin/com/example/clean/DefaultDataBinder.kt
+++ app/src/main/kotlin/com/example/clean/DefaultDataBinder.kt
@@ -3,15 +3,16 @@ package com.example.clean
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
 import android.view.View
+import com.example.clean.screens.ScreenEvents
 import com.example.domain.UiState
 
 class DefaultDataBinder : DataBinder {
-    override fun bind(view: View, state: UiState, events: ClarifyingQuestionsEvents) {
+    override fun bind(view: View, state: UiState, events: ScreenEvents) {
         DataBindingUtil.bind<ViewDataBinding>(view).run {
             setVariable(BR.viewModel, state)
             setVariable(BR.events, events)
             //do not run this on each iteration?
-            //executePendingBindings()
+            executePendingBindings()
         }
     }
 }
