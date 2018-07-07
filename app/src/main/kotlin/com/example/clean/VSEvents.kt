55ee42bdbc41a661428b5f8aabfae0f59d04fe0e
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2018-07-07 19:10:02 +0300

    Removed redundant files

--- app/src/main/kotlin/com/example/clean/VSEvents.kt
+++
@@ -1,8 +0,0 @@
-package com.example.clean
-
-import kotlin.annotation.AnnotationRetention.RUNTIME
-import kotlin.annotation.AnnotationTarget.CLASS
-
-@Target(CLASS)
-@Retention(RUNTIME)
-annotation class VSEvents(val handler: String)
