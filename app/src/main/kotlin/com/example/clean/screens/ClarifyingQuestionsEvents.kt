21a099c445bb0eae9311acb26ccca5e2acc7cc86
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2018-07-12 12:57:35 +0300

    Cleaned up redundant code

--- app/src/main/kotlin/com/example/clean/screens/ClarifyingQuestionsEvents.kt
+++ app/src/main/kotlin/com/example/clean/screens/ClarifyingQuestionsEvents.kt
@@ -1,71 +1,16 @@
 package com.example.clean.screens
 
-import com.example.clean.BR
-import com.example.clean.ObservableProperty
-import com.example.clean.R
 import com.example.domain.submitProposal.ClarifyingQuestions
-import me.tatarka.bindingcollectionadapter2.ItemBinding
 
 class ClarifyingQuestionsEvents(
         val clarifyingQuestions: ClarifyingQuestions
 
 ) : OnItemClickListener {
 
-    val itemBinding: ItemBinding<ClarifyingQuestions.QuestionViewState>
-
-    fun blah() {
-        println("blah")
-    }
-
-
     override fun onItemClick(item: ClarifyingQuestions.QuestionViewState?, text: CharSequence) {
         clarifyingQuestions.fromEvent(ClarifyingQuestions.Command.UpdateAnswer(item!!.id, text.toString()))
-
-//        println("item click")
-        //clarifyingQuestions.fromEvent(ClarifyingQuestions.Command.UpdateAnswer(item.id, item.onChanged.get()
-    }
-
-    val onTextChanged = ObservableProperty<String>()
-
-    //val onChanged = PublishSubject.create<MainActivity.QuestionViewStateEvents>()
-
-    init {
-        itemBinding = ItemBinding.of<ClarifyingQuestions.QuestionViewState>(BR.v, R.layout.clarifying_question_item)
-                .bindExtra(BR.listener, this)
-                .bindExtra(BR.some, Some())
-
-        println("init CLE")
-        onTextChanged.observe().subscribe {
-            println("00000000CQ change $it");
-
-            //clarifyingQuestions.fromEvent(ClarifyingQuestions.Command.UpdateAnswer(it.id, it.onChanged.get()
-            //coverLetter.fromEvent(CoverLetter.Command.UpdateCoverLetter(it))
-        }
-
-        /*onChanged.onErrorResumeNext(
-                Observable.empty<MainActivity.QuestionViewStateEvents>()
-        ).subscribe {
-            println("CUMCHNAGE")
-
-            //activity.runOnUiThread {
-                clarifyingQuestions.fromEvent(ClarifyingQuestions.Command.UpdateAnswer(it.wrapped.id, it.onChanged.get()
-                        ?: ""))
-            //}
-        }*/
-    }
-
-
-
-
-}
-
-class Some :Runnable {
-    override fun run() {
-        println("dsfads")
     }
 }
-
-val some = { println("23421")}
 
 interface OnItemClickListener {
     fun onItemClick(item: ClarifyingQuestions.QuestionViewState?, text: CharSequence)
--- domain/src/main/kotlin/com/example/domain/submitProposal/ProposalSummary.kt
+++ domain/src/main/kotlin/com/example/domain/submitProposal/ProposalSummary.kt
@@ -1,77 +1,9 @@
 package com.example.domain.submitProposal
 
-import com.example.domain.UiCommand
-import com.example.domain.UiComponent
 import com.example.domain.UiResult
 import com.example.domain.UiState
 import com.example.domain.common.JobTypeViewState
-import com.example.domain.models.ItemOpportunity
-import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
-
-/*class ProposalSummaryComponent(
-        *//*val proposeTermsResults: Observable<UiResult>,
-        val coverLetterResults: Observable<UiResult>,
-        val pineappleQuestionsResults: Observable<UiResult>*//*
-        val allResults: Observable<UiResult> //+ attachments! or split?
-) : UiComponent {
-
-    val results = PublishSubject.create<UiResult>()
-
-    override fun processCommands(commands: Observable<UiCommand>) {
-        commands.compose(psProcessor).subscribe(results)
-    }
-
-    override fun produceResults(): Observable<UiResult> {
-        return results.filter { it is ProposalSummaryResult.NavigationRequested || it == ProposalSummaryResult.SubmitProposalRequested }
-    }
-
-    override fun render(): Observable<UiState> {
-        return results.mergeWith(allResults).compose(psReducer)
-    }
-
-    override fun asTransformer(): Observable.Transformer<UiResult, UiState> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-}*/
-
-class ProposalSummary : UiComponent<ProposalSummary.Command, ProposalSummary.Result, SubmitProposal.ViewState> {
-    override fun process(commands: Observable<Command>): Observable<Result> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun render(): Observable<SubmitProposal.ViewState> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    sealed class Command : UiCommand {
-        data class INIT(val itemOpportunity: ItemOpportunity) : Command()
-
-        data class ToggleSubmitEnabled(val enabled: Boolean) : Command()
-    }
-
-
-    sealed class Result : UiResult {
-        internal data class INITResult(val itemOpportunity: ItemOpportunity) : Result()
-        internal data class SubmitEnabled(val enabled: Boolean) : Result()
-
-        data class NavigationRequested(val to: Int) : Result()
-        object SubmitProposalRequested : Result()
-    }
-
-    val psProcessor =
-            ObservableTransformer<UiCommand, UiResult> {
-                it.map {
-                    when (it) {
-                        is Command.INIT -> Result.INITResult(it.itemOpportunity)
-                        is Command.ToggleSubmitEnabled -> Result.SubmitEnabled(it.enabled)
-                        else -> throw IllegalStateException("sdf")
-                    }
-                }
-
-            }
-}
 
 data class JobInfoViewState(
         val title: String,
