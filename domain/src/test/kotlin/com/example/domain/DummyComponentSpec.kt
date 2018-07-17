83688ccf436529e3c74a5fc25c905158c0829c41
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2018-07-17 16:01:39 +0300

    Grouped dependencies in a separate class

--- domain/src/test/kotlin/com/example/domain/DummyComponentSpec.kt
+++ domain/src/test/kotlin/com/example/domain/DummyComponentSpec.kt
@@ -1,12 +1,22 @@
 package com.example.domain
 
 import io.reactivex.Observable
+import io.reactivex.observers.TestObserver
+import io.reactivex.subjects.PublishSubject
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 
+data class Depend(
+        val component: DummyComponent,
+        val results: Observable<DummyComponent.Result>,
+        val scopeExit: PublishSubject<Unit>,
+        val subsciption: TestObserver<DummyComponent.Result>
+)
+
 class DummyComponentSpec : Spek({
+
 
     given("BS") {
 
@@ -16,8 +26,32 @@ class DummyComponentSpec : Spek({
 
             println("sub")
             val command = DummyComponent.Command.DATA("some")
-            Observable.just(command).compose(c).test()
+            Observable.just(command).compose(c)
         }
+
+        val scoped by memoized {
+            val scoped = PublishSubject.create<Unit>()
+            sub.takeUntil(scoped)
+            scoped
+        }
+
+        val deps by memoized {
+            val command = DummyComponent.Command.DATA("some")
+
+            val component = DummyComponent()
+            val scopeExit = PublishSubject.create<Unit>()
+            val results = Observable.just(command).compose(component)
+            val subscription = results.firstOrError().test()//.takeUntil(scopeExit).test()
+
+            Depend(
+                    component,
+                    results,
+                    scopeExit,
+                    subscription
+            )
+        }
+
+
 
         describe("client uses component as one-shot start command") {
             beforeEachTest {
@@ -26,15 +60,22 @@ class DummyComponentSpec : Spek({
             }
 
             it("should receive processor results") {
-                sub.assertValue(DummyComponent.Result.Empty)
+                sub.test().assertValue(DummyComponent.Result.Empty)
 
 
             }
 
-            it("should end processor subscription") {
-                sub.assertComplete()
+            describe("scope of component is exited") {
+                beforeEachTest {
+                    println("scoped")
+                    //scoped.onNext(Unit)//.delay(100, TimeUnit.MILLISECONDS)
+                    deps.component.render().takeUntil(deps.scopeExit).subscribe()
+                    //deps.scopeExit.onNext(Unit)
+                }
 
-
+                it("should end processor subscription") {
+                    deps.subsciption.assertComplete()
+                }
             }
 
             describe("render") {
@@ -65,6 +106,9 @@ class DummyComponentSpec : Spek({
                 it("should process to update view state") {
                     val testSub = c.render().test()
 
+                    testSub.assertOf {
+                    }
+
                     testSub.assertValue(DummyComponent.ViewState())
 
                 }
@@ -78,6 +122,27 @@ class DummyComponentSpec : Spek({
                 it("should dispose 'upstream'") {
 
                 }
+            }
+
+            describe("client unsubscribes from render by leaving scope") {
+                beforeEachTest {
+                    val command = DummyComponent.Command.DATA("some")
+                    deps.component.standalone(command)
+
+                    deps.component.render().takeUntil(deps.scopeExit).subscribe()
+
+                    deps.subsciption.dispose()
+
+                    deps.scopeExit.onNext(Unit)
+
+
+                }
+
+                it("should not accept extra commands") {
+                    val command = DummyComponent.Command.DATA("some")
+                    assert(!deps.component.sendCommand(command))
+                }
+
             }
         }
     }
