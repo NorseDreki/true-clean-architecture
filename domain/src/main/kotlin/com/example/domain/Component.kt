05f8697bbb29343b8639c4c85af6e5dd405d7688
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2018-07-18 14:46:56 +0300

    Added draft for `EmbeddedComponent`

--- domain/src/main/kotlin/com/example/domain/Component.kt
+++ domain/src/main/kotlin/com/example/domain/Component.kt
@@ -1,6 +1,7 @@
 package com.example.domain
 
 import io.reactivex.Observable
+import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
 import io.reactivex.subjects.PublishSubject
 
@@ -16,6 +17,9 @@ interface Component<C : UiCommand, R : UiResult, S : UiState> {
 
 fun <C : UiCommand, R : UiResult, S : UiState> Component<C, R, S>.asStandalone(startingCommand: C) =
         StandaloneComponent(this, startingCommand)
+
+fun <C : UiCommand, R : UiResult, S : UiState> Component<C, R, S>.asEmbedded() =
+        EmbeddedComponent(this)
 
 
 class StandaloneComponent<C : UiCommand, R : UiResult, S : UiState>
@@ -34,6 +38,24 @@ internal constructor(
         //or subject will be auto-disposed?
         //.doOnDispose { commands.onComplete() }
     }
+}
+
+class EmbeddedComponent<C : UiCommand, R : UiResult, S : UiState>
+internal constructor(
+        private val component: Component<C, R, S>
+) :
+        Component<C, R, S> by component,
+        ObservableTransformer<C, R>,
+        ViewStateProducer<S> {
+
+    override fun apply(upstream: Observable<C>): ObservableSource<R> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun viewStates(): Observable<S> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
 
 fun <C : UiCommand, R : UiResult, S : UiState> Component<C, R, S>.extraCommand(command: C): Boolean {
