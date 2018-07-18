20f0042295732923c629f72cd9c060d9abed1165
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2018-07-18 13:12:43 +0300

    Added `asStandalone` translation

--- domain/src/main/kotlin/com/example/domain/Component.kt
+++ domain/src/main/kotlin/com/example/domain/Component.kt
@@ -9,13 +9,21 @@ abstract class Component<C : UiCommand, R : UiResult, S : UiState> {
 
     internal val commands = PublishSubject.create<C>()
 
-    protected abstract val processor: ObservableTransformer<C, R>
+    internal abstract val processor: ObservableTransformer<C, R>
 
     protected abstract val reducer: ObservableTransformer<R, S>
 
     internal lateinit var results: Observable<R>
 
 
+}
+
+fun <C : UiCommand, R : UiResult, S : UiState> Component<C, R, S>.asStandalone() =
+        StandaloneComponent(this)
+
+
+class StandaloneComponent<C : UiCommand, R : UiResult, S : UiState>(val component: Component<C, R, S>) :
+        Component<C, R, S> by component {
 }
 
 fun <C : UiCommand, R : UiResult, S : UiState> Component<C, R, S>.extraCommand(command: C): Boolean {
@@ -36,4 +44,23 @@ fun <C : UiCommand, R : UiResult, S : UiState> Component<C, R, S>.extraCommand(c
     commands.onNext(command)
 
     return true
+}
+
+fun test() {
+    //val c =
+}
+
+interface Base {
+    fun print()
+}
+
+class BaseImpl(val x: Int) : Base {
+    override fun print() { print(x) }
+}
+
+class Derived(b: Base) : Base by b
+
+fun main(args: Array<String>) {
+    val b = BaseImpl(10)
+    Derived(b).print()
 }
