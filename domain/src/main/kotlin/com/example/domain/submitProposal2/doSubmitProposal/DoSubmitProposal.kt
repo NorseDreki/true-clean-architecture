10a4fbd5acc6110147d0db6533a618e980da997f
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2018-07-26 14:02:51 +0300

    Cleaned up

--- domain/src/main/kotlin/com/example/domain/submitProposal2/doSubmitProposal/DoSubmitProposal.kt
+++ domain/src/main/kotlin/com/example/domain/submitProposal2/doSubmitProposal/DoSubmitProposal.kt
@@ -1,5 +1,6 @@
 package com.example.domain.submitProposal2.doSubmitProposal
 
+import com.example.domain.DataCommand
 import com.example.domain.UiCommand
 import com.example.domain.UiResult
 import com.example.domain.UiState
@@ -7,24 +8,23 @@ import com.example.domain.framework.ExtraCommandsComponent
 import com.example.domain.framework.Navigator
 import com.example.domain.models.ItemOpportunity
 import com.example.domain.submitProposal2.common.DialogState
+import com.example.domain.submitProposal2.doSubmitProposal.DoSubmitProposal.*
 import com.example.domain.submitProposal2.doSubmitProposal.proposalConfirmation.ProposalConfirmation
 
 class DoSubmitProposal(
-        val navigator: Navigator,
+        navigator: Navigator,
         val proposalConfirmation: ProposalConfirmation
-): ExtraCommandsComponent<DoSubmitProposal.Command, DoSubmitProposal.Result, DoSubmitProposal.ViewState>() {
-    override val processor = Processor(navigator, proposalConfirmation)
-    override val reducer = Reducer()
-
+): ExtraCommandsComponent<Command, Result, ViewState>() {
 
     sealed class Command : UiCommand {
-        data class DATA(val itemOpportunity: ItemOpportunity) : Command()
+        data class DATA(val itemOpportunity: ItemOpportunity) : Command(), DataCommand
+
         data class ToggleSubmitEnabled(val enabled: Boolean) : Command()
         object DoSubmit : Command()
     }
 
     sealed class Result : UiResult {
-        data class SubmitStatus(val enabled: Boolean) : Result()
+        data class SubmitEnabled(val enabled: Boolean) : Result()
 
         object InProgress : Result()
         data class Success(val response: String) : Result()
@@ -34,17 +34,12 @@ class DoSubmitProposal(
     //error cases: already submitted, 500, 503
 
     data class ViewState(
-            val isSubmitEnabled: Boolean,
-            val status: DialogState
-    ) : UiState {
-        companion object {
-            /*fun initial() = SubmitProposalViewState(
-                    CoverLetterViewState("2312", false),
-                    PineappleQuestionsViewState(listOf())
-            )*/
-        }
-    }
+            val isSubmitEnabled: Boolean = false,
+            val status: DialogState = DialogState.Dismissed
+    ) : UiState
+
+    override val processor = Processor(navigator, proposalConfirmation)
+    override val reducer = Reducer()
 }
 //handle job updates to update job title in Proposal, and questions
 //alert if both changed?
-
--- domain/src/main/kotlin/com/example/domain/submitProposal2/doSubmitProposal/Reducer.kt
+++ domain/src/main/kotlin/com/example/domain/submitProposal2/doSubmitProposal/Reducer.kt
@@ -1,36 +1,36 @@
 package com.example.domain.submitProposal2.doSubmitProposal
 
 import com.example.domain.submitProposal2.common.DialogState
+import com.example.domain.submitProposal2.doSubmitProposal.DoSubmitProposal.Result
+import com.example.domain.submitProposal2.doSubmitProposal.DoSubmitProposal.ViewState
 import io.reactivex.Observable
-import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
 
-class Reducer : ObservableTransformer<DoSubmitProposal.Result, DoSubmitProposal.ViewState> {
+class Reducer : ObservableTransformer<Result, ViewState> {
+    override fun apply(upstream: Observable<Result>) =
 
-    override fun apply(upstream: Observable<DoSubmitProposal.Result>): ObservableSource<DoSubmitProposal.ViewState> {
-        return upstream.scan(DoSubmitProposal.ViewState(false, DialogState.Dismissed)) { state, result ->
-            when (result) {
-                is DoSubmitProposal.Result.SubmitStatus -> state.copy(isSubmitEnabled = result.enabled)
-                DoSubmitProposal.Result.InProgress -> state.copy(
-                        status = DialogState.Progress(null, "In Progress")
-                )
-                is DoSubmitProposal.Result.Success -> {
-
-                    state.copy(
-                            //isSubmitEnabled = false,
-                            status = DialogState.Dismissed
+            upstream.scan(ViewState()) { state, result ->
+                when (result) {
+                    is Result.SubmitEnabled -> {
+                        state.copy(isSubmitEnabled = result.enabled)
+                    }
+                    Result.InProgress -> state.copy(
+                            status = DialogState.Progress(null, "In Progress")
+                    )
+                    is Result.Success -> {
+                        state.copy(
+                                //isSubmitEnabled = false,
+                                status = DialogState.Dismissed
+                        )
+                    }
+                    is Result.Error -> state.copy(
+                            status = DialogState.Alert(
+                                    "title",
+                                    "message",
+                                    "Retry",
+                                    "Cancel"
+                            )
                     )
                 }
-                is DoSubmitProposal.Result.Error -> state.copy(
-                        status = DialogState.Alert(
-                                "title",
-                                "message",
-                                "Retry",
-                                "Cancel"
-                        )
-                )
-            }
-        }
-    }
-
+            }!!
 }
