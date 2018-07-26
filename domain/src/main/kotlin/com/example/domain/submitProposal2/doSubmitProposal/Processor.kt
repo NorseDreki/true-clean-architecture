c8ae3ca7648130bb5a1f7cc1041b63b4b8c0eaa1
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2018-07-26 14:07:22 +0300

    Refactored do submit proposal processor to use `withData`

--- domain/src/main/kotlin/com/example/domain/submitProposal2/doSubmitProposal/Processor.kt
+++ domain/src/main/kotlin/com/example/domain/submitProposal2/doSubmitProposal/Processor.kt
@@ -1,68 +1,50 @@
 package com.example.domain.submitProposal2.doSubmitProposal
 
 import com.example.domain.framework.Navigator
+import com.example.domain.framework.withData
 import com.example.domain.models.Proposal
+import com.example.domain.submitProposal2.doSubmitProposal.DoSubmitProposal.Command
+import com.example.domain.submitProposal2.doSubmitProposal.DoSubmitProposal.Result
 import com.example.domain.submitProposal2.doSubmitProposal.proposalConfirmation.ProposalConfirmation
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
-import io.reactivex.functions.BiFunction
 import java.util.concurrent.TimeUnit
 
 class Processor(
         val navigator: Navigator,
         val proposalConfirmation: ProposalConfirmation
-) : ObservableTransformer<DoSubmitProposal.Command, DoSubmitProposal.Result> {
+) : ObservableTransformer<Command, Result> {
 
-    override fun apply(upstream: Observable<DoSubmitProposal.Command>): Observable<DoSubmitProposal.Result> {
-        return upstream.publish { shared ->
-            println("DSP COMMAND $shared")
-            Observable.combineLatest<Proposal, DoSubmitProposal.Command, Pair<Proposal, DoSubmitProposal.Command>>(
+    override fun apply(upstream: Observable<Command>) =
+            upstream
+                    .compose(withData)
+                    .flatMap {
+                        val command = it.currentCommand as Command
+                        val proposal = (it.dataCommand as Command.DATA).itemOpportunity.proposal
 
-                    shared.ofType(DoSubmitProposal.Command.DATA::class.java).map {
-                        /*navigator.display(
-                                proposalConfirmation,
-                                ProposalConfirmation.Command.DATA(it.itemOpportunity)
-                        )*/
-                        it.itemOpportunity.proposal
+                        when (command) {
+                            is Command.ToggleSubmitEnabled ->
+                                Observable.just(Result.SubmitEnabled(command.enabled))
+                            Command.DoSubmit -> {
+                                doSubmit(proposal)
+                            }
+                            is Command.DATA -> Observable.empty()
+                        }
+                    }!!
 
-                    },
-                    shared.filter { it !is DoSubmitProposal.Command.DATA },
-
-                    BiFunction { proposal, command ->
-                        Pair(proposal, command)
-                    }
-            ).flatMap {
-                val command = it.second
-                val proposal = it.first
-                when (command) {
-                    is DoSubmitProposal.Command.ToggleSubmitEnabled ->
-                        Observable.just(DoSubmitProposal.Result.SubmitStatus(command.enabled))
-                    is DoSubmitProposal.Command.DoSubmit -> {
-                        doSubmit(proposal)
-                    }
-                    is DoSubmitProposal.Command.DATA -> throw IllegalStateException("dsf")
-                }
-            }.cast(DoSubmitProposal.Result::class.java)
-        }
-    }
-
-    private fun doSubmit(proposal: Proposal): Observable<DoSubmitProposal.Result> {
-
-        println("DO SUBMIT")
+    private fun doSubmit(proposal: Proposal): Observable<Result> {
         val api: Api? = SomeApi()
 
         return api!!.submitProposal("123", "dsf")
                 .delay(3, TimeUnit.SECONDS)
-                .map(DoSubmitProposal.Result::Success)
+                .map(Result::Success)
                 .doOnNext {
                     navigator.display(
                             proposalConfirmation,
                             ProposalConfirmation.Command.DATA("some"))
                 }
-                .cast(DoSubmitProposal.Result::class.java)
-                .onErrorReturn(DoSubmitProposal.Result::Error)
-                .startWith(DoSubmitProposal.Result.InProgress)
+                .cast(Result::class.java)
+                .onErrorReturn(Result::Error)
+                .startWith(Result.InProgress)
     }
-
-
 }
