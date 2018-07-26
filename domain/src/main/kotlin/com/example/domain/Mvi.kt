6bb67537e9380fcbadead74bc3f31455f1d6ea07
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2018-07-26 14:06:53 +0300

    Extracted `ProcessorState` adn `withData`

--- domain/src/main/kotlin/com/example/domain/Mvi.kt
+++ domain/src/main/kotlin/com/example/domain/Mvi.kt
@@ -14,6 +14,7 @@ interface Result
 
 interface ScreenComponent
 
+interface DataCommand : UiCommand
 
 interface UiEvent
 
---
+++ domain/src/main/kotlin/com/example/domain/framework/ProcessorState.kt
@@ -0,0 +1,27 @@
+package com.example.domain.framework
+
+import com.example.domain.DataCommand
+import com.example.domain.UiCommand
+import io.reactivex.ObservableTransformer
+import io.reactivex.functions.BiFunction
+
+data class ProcessorState(
+        val dataCommand: DataCommand,
+        val currentCommand: UiCommand
+)
+
+val withData =
+        ObservableTransformer<UiCommand, ProcessorState> {
+            it.publish { shared ->
+                shared.withLatestFrom(
+                        shared
+                                .firstOrError()
+                                .cast(DataCommand::class.java)
+                                .toObservable(),
+
+                        BiFunction<UiCommand, DataCommand, ProcessorState> { t1, t2 ->
+                            ProcessorState(t2, t1)
+                        }
+                )
+            }
+        }
