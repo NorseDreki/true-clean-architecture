99efc317682a7cbc19be872c11a2c9896fa46d9e
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2018-08-01 11:14:58 +0300

    Implemented reducer

---
+++ domain/src/main/kotlin/com/example/domain/submitProposal2/proposeTip/Reducer.kt
@@ -0,0 +1,30 @@
+package com.example.domain.submitProposal2.proposeTip
+
+import com.example.domain.submitProposal2.proposeTip.ProposeTip.Result
+import com.example.domain.submitProposal2.proposeTip.ProposeTip.ViewState
+import io.reactivex.Observable
+import io.reactivex.ObservableTransformer
+
+class Reducer : ObservableTransformer<Result, ViewState> {
+
+    override fun apply(upstream: Observable<Result>) =
+
+            upstream.scan(ViewState()) { state, result ->
+                when (result) {
+                    Result.TipNotEntered ->
+                            state.copy(tip = 0, isRangeError = false)
+
+                    is Result.TipValid ->
+                        state.copy(tip = result.tip, isRangeError = false)
+
+                    is Result.TipRangeExceeded ->
+                        state.copy(tip = result.tip, isRangeError = true, minTip = result.min, maxTip = result.max)
+
+                    is Result.FeeCalculated ->
+                            state.copy(tipWithFee = result.tipWithFee, isFeeLoading = false)
+
+                    //to emit nothing is better
+                    else -> state
+                }
+            }!!
+}
