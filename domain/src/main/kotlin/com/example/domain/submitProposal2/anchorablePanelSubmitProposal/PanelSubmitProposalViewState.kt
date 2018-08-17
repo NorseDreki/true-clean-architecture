0a7d8cb8198e251d4cb6a8e254370a662033b362
Alexey Dmitriev <mr.alex.dmitriev@gmail.com>
2018-08-17 15:35:51 +0300

    Added panel layout

--- domain/src/main/kotlin/com/example/domain/submitProposal2/anchorablePanelSubmitProposal/PanelSubmitProposalViewState.kt
+++ domain/src/main/kotlin/com/example/domain/submitProposal2/anchorablePanelSubmitProposal/PanelSubmitProposalViewState.kt
@@ -3,7 +3,7 @@ package com.example.domain.submitProposal2.anchorablePanelSubmitProposal
 import com.example.domain.submitProposal2.anchorablePanel.AnchorablePanelViewState
 import com.example.domain.submitProposal2.anchorablePanelToolbar.AnchorablePanelToolbarViewState
 
-data class AnchorablePanelSubmitProposalViewState(
+data class PanelSubmitProposalViewState(
         val anchorablePanel: AnchorablePanelViewState,
         val anchorablePanelToolbar: AnchorablePanelToolbarViewState,
         val submitProposalPages: SubmitProposalPages
